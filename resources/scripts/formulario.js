7ea8b5a8590e9861d3cf0c9c0ae013a6a3a6fde0
Guilherme Castro <guibkk@gmail.com>
2024-10-09 20:58:30 -0300

    Teste de e-mail

--- resources/scripts/formulario.js
+++ resources/scripts/formulario.js
@@ -1,3 +1,8 @@
+const form = document.getElementById('formContato')
+form.addEventListener('submit', e => {
+    e.preventDefault()
+})
+
 const tel = document.getElementById('celular') // Seletor do campo de telefone
 
 tel.addEventListener('keypress', (e) => mascaraTelefone(e.target.value)) // Dispara quando digitado no campo
@@ -8,4 +13,27 @@ const mascaraTelefone = (valor) => {
     valor = valor.replace(/^(\d{2})(\d)/g, "($1) $2")
     valor = valor.replace(/(\d)(\d{4})$/, "$1-$2")
     tel.value = valor // Insere o(s) valor(es) no campo
+}
+
+
+function sendEmail() {
+    let nome = document.getElementById('nomeCompleto').value;
+    let CPF = document.getElementById('cpfCnpj').value;;
+    let nascimento = document.getElementById('dtNascimento').value;;
+    let celular = document.getElementById('celular').value;
+    let email = document.getElementById('email').value;;
+    let termoAceite = document.getElementById('autorizacao').value;
+
+    let corpo = 
+            ` Nome Completo: ${nome};%0D
+                CPFCNPJ: ${CPF};
+                Dt. Nascimento: ${nascimento};
+                Celular: ${celular};
+                email: ${email};
+                Autorizo o cadastro dos meus dados para que um consultor da Nossagente entre em contato comigo.
+            `
+    let teste = document.getElementById('teste');   
+    teste.href= 'mailto:guibkk@gmail.com?subject=Formulário%20NossaGente&body=' + corpo
+    teste.click();
+ 
 }
