66777f200c7ca792ee5fdbb70ac622cca07ac40b
Guilherme Castro <guibkk@gmail.com>
2024-10-09 21:27:09 -0300

    AJustes formulários

--- resources/scripts/formulario.js
+++ resources/scripts/formulario.js
@@ -15,6 +15,24 @@ const mascaraTelefone = (valor) => {
     tel.value = valor // Insere o(s) valor(es) no campo
 }
 
+const cnpj = document.getElementById('cpfCnpj');
+cnpj.addEventListener('keypress', (e) => mascaraCNPJ(e.target.value)) // Dispara quando digitado no campo
+cnpj.addEventListener('change', (e) => mascaraCNPJ(e.target.value)) // Dispara quando autocompletado o campo
+
+const mascaraCNPJ = (valor) => {
+    let tamanho = cnpj.length;
+    if(tamanho <= 11){
+        valor = valor.replace(/\D/g, "")
+        valor = valor.replace(/^(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4")
+        cnpj.value = valor // Insere o(s) valor(es) no campo
+    } else {
+        valor = valor.replace(/\D/g, "")
+        valor = valor.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, "$1.$2.$3/$4-$5")
+        cnpj.value = valor // Insere o(s) valor(es) no campo
+    }
+    
+}
+
 
 function sendEmail() {
     let nome = document.getElementById('nomeCompleto').value;
